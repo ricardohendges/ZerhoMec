88e46083b278cd347d9df5c4b597e0ed30e89616
Maglan Cristiano Bieger <72475023+MaglanCB@users.noreply.github.com>
2020-11-10 22:39:36 -0300

    AddProject
    Adicionado projeto ao git

---
+++ prj/prjTrabalhoFinal.dpr
@@ -0,0 +1,22 @@
+program prjTrabalhoFinal;
+
+uses
+  Vcl.Forms,
+  ufrmPrincipal in '..\src\ufrmPrincipal.pas' {frmPrincipal},
+  ufrmBaseCrud in '..\src\Framework\Base\ufrmBaseCrud.pas' {frmBaseCrud},
+  ufrmBaseDM in '..\src\Framework\DataModule\ufrmBaseDM.pas' {frmBaseDM: TDataModule},
+  Sistema.Utils.Forms in '..\src\Framework\Utils\Sistema.Utils.Forms.pas',
+  Sistema.Utils.Grid in '..\src\Framework\Utils\Sistema.Utils.Grid.pas',
+  Sistema.Utils.Helpers in '..\src\Framework\Utils\Sistema.Utils.Helpers.pas',
+  Sistema.Utils.Types in '..\src\Framework\Utils\Sistema.Utils.Types.pas',
+  ufrmCadLogin in '..\src\frmCadLogin\ufrmCadLogin.pas' {frmCadLogin};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.CreateForm(TfrmBaseDM, frmBaseDM);
+  Application.Run;
+end.
