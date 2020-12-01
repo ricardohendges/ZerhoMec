7020eb2668a37cf3824a44e749df141f07ac79ee
DjonatanWillenz <66548419+DjonatanWillenz@users.noreply.github.com>
2020-12-01 20:02:44 -0300

    DJO-ajuste merge

--- prj/prjTrabalhoFinal.dpr
+++ prj/prjTrabalhoFinal.dpr
@@ -2,15 +2,18 @@ program prjTrabalhoFinal;
 
 uses
   Vcl.Forms,
-  ufrmPrincipal in '..\src\ufrmPrincipal.pas' {frmPrincipal},
   ufrmBaseCrud in '..\src\Framework\Base\ufrmBaseCrud.pas' {frmBaseCrud},
   ufrmBaseDM in '..\src\Framework\DataModule\ufrmBaseDM.pas' {frmBaseDM: TDataModule},
+  ufrmCadLogin in '..\src\frmCadLogin\ufrmCadLogin.pas' {frmCadLogin},
+  uFrmLoginUsuario in '..\src\frmLoginUsuario\uFrmLoginUsuario.pas' {frmLoginUsuario},
+  Sistema.Utils.Busca in '..\src\Framework\Utils\Sistema.Utils.Busca.pas',
   Sistema.Utils.Forms in '..\src\Framework\Utils\Sistema.Utils.Forms.pas',
   Sistema.Utils.Grid in '..\src\Framework\Utils\Sistema.Utils.Grid.pas',
   Sistema.Utils.Helpers in '..\src\Framework\Utils\Sistema.Utils.Helpers.pas',
   Sistema.Utils.Types in '..\src\Framework\Utils\Sistema.Utils.Types.pas',
-  ufrmCadLogin in '..\src\frmCadLogin\ufrmCadLogin.pas' {frmCadLogin},
-  uFrmLoginUsuario in '..\src\frmLoginUsuario\uFrmLoginUsuario.pas' {frmLoginUsuario};
+  ufrwUtilsForms in '..\src\Framework\Utils\ufrwUtilsForms.pas',
+  ufrmPrincipal in '..\src\ufrmPrincipal.pas' {frmPrincipal},
+  UFRMBusca in '..\src\Framework\Busca\UFRMBusca.pas' {FRMBusca};
 
 {$R *.res}
 
@@ -19,5 +22,6 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfrmBaseDM, frmBaseDM);
   Application.CreateForm(TfrmLoginUsuario, frmLoginUsuario);
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
   Application.Run;
 end.
